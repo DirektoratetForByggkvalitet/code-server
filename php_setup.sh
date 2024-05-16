288dbfec476113509bdeb5cb9badd0c7db0eca87
Eirik Wulff <ew@dibk.no>
2024-05-16 12:05:19 +0200

    Byttet til p7zip o.l. i stedet for zip og 7zip

--- php_setup.sh
+++ php_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update && apt install dialog zip 7zip ca-certificates apt-transport-https software-properties-common lsb-release -y
+apt update && apt install dialog pigz pbzip2 p7zip ca-certificates apt-transport-https software-properties-common lsb-release -y
 add-apt-repository ppa:ondrej/php -y && apt upgrade -y
 apt install -y php8.3-{cli,curl,mysqlnd,gd,opcache,zip,intl,common,bcmath,imagick,xmlrpc,readline,memcached,redis,mbstring,apcu,xml,dom,memcache}
 curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
